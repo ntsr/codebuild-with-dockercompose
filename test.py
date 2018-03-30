f71420951c9b5c86451be1b222d7c5efffd55d00
naoki_tsuru <naoki_tsuru@balmuda.com>
2018-03-30 13:14:37 +0900

    sample user test

--- test.py
+++ test.py
@@ -3,17 +3,27 @@
 
 import unittest
 from localstack.services import infra
+from logzero import logger
+from users import User
+
 
 
 class TestCase(unittest.TestCase):
     def setUp(self):
         infra.start_infra(async=True)
+        User.create_table(wait=True)
 
     def teardown(self):
         infra.stop_infra()
+        User.delete_table()
 
     def test__sample(self):
-        self.assertTrue(False)
+        user = User.new_item(email='sample@sample.com', password='sample')
+        logger.info(user.to_string())
+        user2 = User.get(user.user_id)
+        logger.info(user2.to_string())
+
+        self.assertEqual(user.email, user2.email)
 
 
 if __name__ == '__main__':
---
+++ users.py
@@ -0,0 +1,63 @@
+#!/usr/env python
+# -*- coding: utf-8 -*-
+
+import os
+from datetime import datetime as dt
+from pynamodb.attributes import (
+    UnicodeAttribute, UTCDateTimeAttribute
+)
+from pynamodb.models import Model
+from logzero import logger
+
+AWS_DYNAMO_TABLE_NAME_USERS = os.getenv('AWS_DYNAMO_TABLE_NAME_USERS') or 'test-Users'
+LOCAL_DEBUG = bool(os.getenv('LOCAL_DEBUG')) or False
+ID_PREFIX = 'user'
+
+
+class User(Model):
+    class Meta:
+        table_name = AWS_DYNAMO_TABLE_NAME_USERS
+        write_capacity_units = 1
+        read_capacity_units = 1
+
+        if LOCAL_DEBUG:
+            host = 'http://localhost:4569'
+
+    user_id = UnicodeAttribute(hash_key=True)
+    email = UnicodeAttribute()
+    password = UnicodeAttribute(null=True)
+    created_at = UTCDateTimeAttribute()
+    updated_at = UTCDateTimeAttribute()
+
+
+    @classmethod
+    def new_item(cls, email, password, save=True):
+        now = dt.now()
+        user_id = cls.generate_user_id()
+        user = User(
+            user_id,
+            None,
+            email=email,
+            password=password,
+            created_at=now,
+            updated_at=now
+        )
+
+        if save:
+            try:
+                user.save()
+            except Exception as e:
+                logger.error("save failed")
+                return None
+
+        return user
+
+    @classmethod
+    def to_string(cls):
+        return {
+            "user_id": cls.user_id,
+            "email": cls.email,
+            "password": cls.password,
+            "created_at": cls.created_at,
+            "updated_at": cls.updated_at
+        }
